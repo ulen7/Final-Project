2bf26f5175f9dfa2a5fc505e098a85fb467ce92c
ulen7 <alejandrosm1990@gmail.com>
2023-02-07 18:37:31 -0500

    Styling

--- Js/project.js
+++ Js/project.js
@@ -1,12 +1,16 @@
-$(".header--theme-button").on("click", function() {
+$(".colors-button").on("click", function() {
     var primaryColor = $(this).css("--theme-primary");
     var secondaryColor = $(this).css("--theme-secondary");
+    var backgroundColor = $(this).css("--theme-third");
+    var maintextColor = $(this).css("--theme-fourth");
   
-    $(".header--theme-button").removeClass("active");
+    $(".colors-button").removeClass("active");
     $(this).addClass("active");
   
     $(document.body).css("--primary-color", primaryColor);
     $(document.body).css("--secondary-color", secondaryColor);
+    $(document.body).css("--background-color", backgroundColor);
+    $(document.body).css("--maintext-color", maintextColor);
   });
 
   $(".nav-links--link").on("click", function() {
