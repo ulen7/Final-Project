16e7ba3552ee9c26fdf18dc8302505729526adaf
Alejandro <alejandrosm1990@gmail.com>
2023-02-07 13:29:12 -0500

    starting

---
+++ Js/project.js
@@ -0,0 +1,10 @@
+$(".header--theme-button").on("click", function() {
+    var primaryColor = $(this).css("--theme-primary");
+    var secondaryColor = $(this).css("--theme-secondary");
+  
+    $(".header--theme-button").removeClass("active");
+    $(this).addClass("active");
+  
+    $(document.body).css("--primary-color", primaryColor);
+    $(document.body).css("--secondary-color", secondaryColor);
+  });
