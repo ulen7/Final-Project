35da27980533d7c5db88497b1c0949de0fe8f53d
ulen7 <alejandrosm1990@gmail.com>
2023-02-07 20:44:44 -0500

    Trying to fix iframe theme issues

--- Js/project.js
+++ Js/project.js
@@ -1,16 +1,16 @@
 $(".colors-button").on("click", function() {
     var primaryColor = $(this).css("--theme-primary");
     var secondaryColor = $(this).css("--theme-secondary");
-    var backgroundColor = $(this).css("--theme-third");
-    var maintextColor = $(this).css("--theme-fourth");
+    var bgColor = $(this).css("--theme-bg");
+    var textColor = $(this).css("--theme-text");
   
     $(".colors-button").removeClass("active");
     $(this).addClass("active");
   
     $(document.body).css("--primary-color", primaryColor);
     $(document.body).css("--secondary-color", secondaryColor);
-    $(document.body).css("--background-color", backgroundColor);
-    $(document.body).css("--maintext-color", maintextColor);
+    $(document.body).css("--bg-color", bgColor);
+    $(document.body).css("--text-color", textColor);
   });
 
   $(".nav-links--link").on("click", function() {
