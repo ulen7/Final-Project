0795ff96ce93c3d0f99eb7383b05ecb32856a3a4
ulen7 <alejandrosm1990@gmail.com>
2023-02-25 22:44:09 -0500

    Main changes
    Changes regarding color and creation of about me page

--- Js/project.js
+++ Js/project.js
@@ -11,10 +11,28 @@ $(".colors-button").on("click", function() {
     $(document.body).css("--secondary-color", secondaryColor);
     $(document.body).css("--bg-color", bgColor);
     $(document.body).css("--text-color", textColor);
-    $("iframe_test").contents().find(".content-iframe").css("background-color", "blue");
-  });
+
+    // Get the iframe element
+    var iframe = $("#iframe_1")[0];
+
+    // Get the contents of the iframe
+    var iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
+
+    // Update the CSS styles of the elements inside the iframe
+    $(iframeDoc).find("body").css("--primary-color", primaryColor);
+    $(iframeDoc).find("body").css("--secondary-color", secondaryColor);
+    $(iframeDoc).find("body").css("--bg-color", bgColor);
+    $(iframeDoc).find("body").css("--text-color", textColor);
+});
 
 $(".nav-links--link").on("click", function() {
     $(".nav-links--link").removeClass("active");
     $(this).addClass("active");
+  });
+
+$(document).ready(function() {
+    $('#button-color').click(function() {
+      var color = window.getComputedStyle($('#button-color').get(0)).getPropertyValue('background-color');
+      $('iframe[name="iframe_1"]').contents().find('body').css('background-color', color);
+    });
   });
